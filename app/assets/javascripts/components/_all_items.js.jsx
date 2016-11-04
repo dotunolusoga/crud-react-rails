16b08001ade9d3c9da258bd1d674d157a4ea8a85
Dotun Olusoga <adedotun.olusoga@gmail.com>
2016-11-04 16:27:16 -0500

    CRUD application using Reactjs on the frontend and a Rails api

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -3,18 +3,17 @@ var AllItems = React.createClass({
     this.props.handleDelete(id);
   },
 
-  handleEdit() {
-
+  onUpdate(item) {
+    this.props.onUpdate(item);
   },
 
   render() {
     var items = this.props.items.map((item) => {
       return (
         <div key={item.id}>
-          <h3>{item.name}</h3>
-          <p>{item.description}</p>
-          <button onClick={this.handleDelete.bind(this, item.id)}> Delete </button>
-          <button onClick={this.handleEdit()}> Edit </button>
+          <Item item={item}
+                handleDelete={this.handleDelete.bind(this, item.id)}
+                handleUpdate={this.onUpdate}/>
         </div>
       )
     });
